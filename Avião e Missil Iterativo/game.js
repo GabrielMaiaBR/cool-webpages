129b03ff008629bc34ebc5cc14b62c8ab383d003
GabrielMaiaBR <fgabrielsmaiaf@gmail.com>
2023-09-01 16:44:55 -0300

    base

---
+++ Avião e Missil Iterativo/game.js
@@ -0,0 +1,76 @@
+// Defina variáveis globais
+const canvas = document.getElementById('gameCanvas');
+const ctx = canvas.getContext('2d');
+const toggleSoundButton = document.getElementById('toggleSound');
+let soundEnabled = true;
+
+// Carregue sons
+const explosionSound = new Audio('explosion.mp3');
+const missileSound = new Audio('missile.mp3');
+
+// Defina as posições iniciais do avião e do míssil
+let planeX = canvas.width / 2;
+let planeY = canvas.height / 2;
+let missileX = canvas.width / 2;
+let missileY = canvas.height / 2;
+
+// Função para desenhar o avião
+function drawPlane() {
+    // Implemente o desenho do avião aqui
+}
+
+// Função para desenhar o míssil
+function drawMissile() {
+    // Implemente o desenho do míssil aqui
+}
+
+// Função para atualizar a posição do míssil em direção ao avião
+function updateMissile() {
+    // Implemente a lógica de atualização do míssil aqui
+}
+
+// Função para verificar se o míssil atingiu o avião
+function checkCollision() {
+    // Implemente a verificação de colisão aqui
+}
+
+// Função para lidar com o disparo do míssil
+function fireMissile() {
+    // Implemente a lógica de disparo do míssil aqui
+}
+
+// Função para lidar com o som ligado/desligado
+function toggleSound() {
+    soundEnabled = !soundEnabled;
+    toggleSoundButton.textContent = soundEnabled ? 'Mute Sound' : 'Unmute Sound';
+}
+
+// Adicione um ouvinte de evento para acompanhar o movimento do mouse
+canvas.addEventListener('mousemove', (e) => {
+    planeX = e.clientX - canvas.getBoundingClientRect().left;
+    planeY = e.clientY - canvas.getBoundingClientRect().top;
+});
+
+// Adicione um ouvinte de evento para disparar o míssil com o clique direito
+canvas.addEventListener('contextmenu', (e) => {
+    e.preventDefault();
+    fireMissile();
+});
+
+// Adicione um ouvinte de evento para alternar o som
+toggleSoundButton.addEventListener('click', toggleSound);
+
+// Função principal de animação
+function animate() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    drawPlane();
+    drawMissile();
+    updateMissile();
+    checkCollision();
+
+    requestAnimationFrame(animate);
+}
+
+// Inicie a animação
+animate();
